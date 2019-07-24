88754baa4b2914fefbd2f7a5a520ca79eb72bb99
thowland1 <tomhowland9@gmail.com>
2019-07-24 20:19:51 +0100

    Replace ng-charts with my own graphs

--- ResistanceScores/WebUI/src/app/app.component.ts
+++ ResistanceScores/WebUI/src/app/app.component.ts
@@ -6,27 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'WebUI';
-
-
-
-  xMin = 0;
-  xMax = 75;
-  yMin = 20;
-  yMax = 100;
-  xScale = 200;
-  yScale = 100;
-  xTickScale = 5;
-  yTickScale = 5;
-  xLabelScale = 4;
-  yLabelScale = 3;
-
-  xGridlines = [0, 10,20,30,40,50,60,70, 80, 90, 100];
-  yGridlines = [0,10,20,30,40,50,60,70, 80, 90, 100];
-
-  wins = [0, 0, 0, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 4, 5, 6, 7, 8, 9, 10];
-  played = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
-  gameNo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
-
 }
 
--- ResistanceScores/WebUI/src/app/shared/percentage-graph/percentage-graph.component.ts
+++ ResistanceScores/WebUI/src/app/shared/percentage-graph/percentage-graph.component.ts
@@ -31,87 +31,32 @@ export class PercentageGraphComponent implements OnInit {
           }
       )
   }
+  xMin = 0;
+  xMax = 75;
+  yMin = 20;
+  yMax = 100;
+  xScale = 200;
+  yScale = 100;
+  xTickScale = 5;
+  yTickScale = 5;
+  xLabelScale = 4;
+  yLabelScale = 3;
 
+  xGridlines = [0, 10,20,30,40,50,60,70, 80, 90, 100];
+  yGridlines = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
-  // NG2-CHARTS STUFF
-  public lineChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-    { data: [180, 480, 770, 90, 1000, 270, 400], label: 'Series C', yAxisID: 'y-axis-1' }
-  ];
-  public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  public lineChartOptions: (ChartOptions & { annotation: any }) = {
-    responsive: true,
-    scales: {
-      // We use this empty structure as a placeholder for dynamic theming.
-      xAxes: [{}],
-      yAxes: [
-        {
-          id: 'y-axis-1',
-          position: 'right',
-          gridLines: {
-            color: 'rgba(255,0,0,0.3)',
-          },
-          ticks: {
-            fontColor: 'red',
-          }
-        }
-      ]
-    },
-    annotation: {
-      annotations: [
-        {
-          type: 'line',
-          mode: 'vertical',
-          scaleID: 'x-axis-0',
-          value: 'March',
-          borderColor: 'orange',
-          borderWidth: 2,
-          label: {
-            enabled: true,
-            fontColor: 'orange',
-            content: 'LineAnno'
-          }
-        },
-      ],
-    },
-  };
-  public lineChartColors: Color[] = [
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    },
-    { // dark grey
-      backgroundColor: 'rgba(77,83,96,0.2)',
-      borderColor: 'rgba(77,83,96,1)',
-      pointBackgroundColor: 'rgba(77,83,96,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(77,83,96,1)'
-    },
-    { // red
-      backgroundColor: 'rgba(255,0,0,0.3)',
-      borderColor: 'red',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    }
-  ];
-  public lineChartLegend = true;
-  public lineChartType = 'line';
+  xData = [1, 2, 3, 4];
 
-  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
-    console.log(event, active);
+  get yData(): number[] {
+    return [30,70,20,40]
   }
 
-  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
-    console.log(event, active);
-  }
 
+  public get firstDataline(): { xData: number[], yData: number[] } {
+    if (this.graphData === undefined) return { xData: [], yData: [] };
+    const xData = this.graphData.graphPlayers[0].graphPoints.map(gp => 100 * gp.wins / gp.totalGames);
+    const yData = this.graphData.graphPlayers[0].graphPoints.map(gp => gp.totalGames);
+    return {xData: xData, yData: yData}
+  }
 
 }
